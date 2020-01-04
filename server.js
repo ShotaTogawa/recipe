59b0e71b20798153104647ef8ff284ddb10f93d2
ShotaTogawa <s.togawa0112@gmail.com>
2020-01-03 23:49:25 -0800

    connected db

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "variables.env" });
+
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error(err));
 
 const PORT = process.env.PORT || 4444;
 
