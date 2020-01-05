ac88d9d54dd995d9e847354947e65c338fd1a5f4
ShotaTogawa <s.togawa0112@gmail.com>
2020-01-05 07:19:45 -0800

    added signout func

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { NavLink } from "react-router-dom";
+import Signout from "./Auth/Signout";
 
 const Navbar = ({ session }) => {
   return (
@@ -31,7 +32,7 @@ const NavbarAuth = ({ session }) => (
         <NavLink to="/profile">Profile</NavLink>
       </li>
       <li>
-        <button>Signout</button>
+        <Signout />
       </li>
     </ul>
     <h4>
